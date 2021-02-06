43c47cd2b787acd7be60078a8b645877df6c2f65
gamz <gamzabaw@gmail.com>
2021-02-06 21:43:21 +0900

    Add rpc example

---
+++ examples/rpc/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/sangwonl/mqrpc"
+)
+
+type pingData struct {
+	Peer string
+	Ts   int64
+}
+
+func main() {
+	const AmqpURI = "amqp://mqrpc:mqrpc@localhost:5672/"
+	const Namespace = "examples.rpc"
+	peerName := os.Args[1]
+
+	svc, err := mqrpc.NewMqService(AmqpURI, Namespace, peerName)
+	if err != nil {
+		panic(err)
+	}
+
+	if peerName == "server" {
+		svc.AddHandler("ping", func(ctx *mqrpc.Context) interface{} {
+			var cliPing pingData
+			json.Unmarshal(ctx.GetMessage().Payload, &cliPing)
+			fmt.Printf("Received ping from %s: %d\n", cliPing.Peer, cliPing.Ts)
+
+			return pingData{Peer: peerName, Ts: time.Now().Unix()}
+		})
+	} else {
+		go func() {
+			msgClient := mqrpc.DefaultMessageService{MqService: svc}
+
+			for {
+				time.Sleep(2 * time.Second)
+
+				cliPing := pingData{Peer: peerName, Ts: time.Now().Unix()}
+				resp, _ := msgClient.Request("server", "ping", &cliPing, 0)
+
+				var srvPing pingData
+				json.Unmarshal(resp.Payload, &srvPing)
+
+				fmt.Printf("Received ping from %s: %d\n", srvPing.Peer, srvPing.Ts)
+			}
+		}()
+	}
+
+	svc.Run(false)
+}
